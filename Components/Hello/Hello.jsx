8eff1ac1d3fb0de2b205afd5c06a220b4ad9ac56
REW-Obie <egwim.obie@realestatewebmasters.com>
2016-04-06 11:15:26 -0700

    update component: add class to test css modules

--- Components/Hello/Hello.jsx
+++ Components/Hello/Hello.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styles from './Hello.css';
+// require('./Hello.css');
 
 class Hello extends React.Component{
    render(){
-     return <h1>Hello</h1>
+     return <h1 className={styles.test}>Hello</h1>
    }
 }
 
