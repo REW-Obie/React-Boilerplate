114b93d17d380737637d8c57c13fb1dc1c78c14f
REW-Obie <egwim.obie@realestatewebmasters.com>
2016-04-06 11:19:10 -0700

    add comments

--- default.js
+++ default.js
@@ -2,14 +2,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//imoprt individual Components
+//import individual Components
+//import ComponentName/ from './PATH/TO/Component.jsx';
 import Hello from './Components/Hello/Hello.jsx';
 import World from './Components/World/World.jsx';
 
 //Define constants for each Component
+//const variableName = document.querySelector('#dom-node');
 const hello = document.querySelector('#hello');
 const world = document.querySelector('#world');
 
 //Render each Component to the DOM
+//ReactDOM.render(<Component/>, dom-node);
 ReactDOM.render(<Hello />, hello);
 ReactDOM.render(<World />, world);
